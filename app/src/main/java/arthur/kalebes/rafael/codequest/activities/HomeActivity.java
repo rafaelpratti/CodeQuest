b62e7385b32c756ad29eb9398cc26b59985e7d2e
20221tiimi0106 <rprattidettmann@gmail.com>
2024-12-10 09:10:38 -0300

    nav menu estranhão

--- app/src/main/java/arthur/kalebes/rafael/codequest/activities/HomeActivity.java
+++ app/src/main/java/arthur/kalebes/rafael/codequest/activities/HomeActivity.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import arthur.kalebes.rafael.codequest.R;
 
-public class CursosActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cursos);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
