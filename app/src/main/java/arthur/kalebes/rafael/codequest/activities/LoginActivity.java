21678d13d9cb307b9c027dbca56535cf640c58ac
20221TIIMI0190 <kalebe.olegario1850@gmail.com>
2024-12-19 12:22:16 -0300

    fragments mudando

--- app/src/main/java/arthur/kalebes/rafael/codequest/activities/LoginActivity.java
+++ app/src/main/java/arthur/kalebes/rafael/codequest/activities/LoginActivity.java
@@ -1,12 +1,18 @@
 package arthur.kalebes.rafael.codequest.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import arthur.kalebes.rafael.codequest.R;
 
@@ -21,6 +27,20 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etEmail = findViewById(R.id.etEmailLogin);
+                String email = etEmail.getText().toString();
+                if(email.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Login não preenchido", Toast.LENGTH_LONG);
+
+                }
+
+            }
         });
     }
 }
--- app/src/main/java/arthur/kalebes/rafael/codequest/activities/MainActivity.java
+++ app/src/main/java/arthur/kalebes/rafael/codequest/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package arthur.kalebes.rafael.codequest.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,18 +10,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import arthur.kalebes.rafael.codequest.R;
+import arthur.kalebes.rafael.codequest.util.Config;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Se o usuário ainda não logou, então não existe informação de login guardada na app.
+        // Então a app é redirecionada para a tela de login.
+        if(Config.getLogin(MainActivity.this).isEmpty()) {
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
+        }
+        // Se o usuário já logou, então a informação de login está guardada na app. Então
+        // a app é redirecionada para a tela principal da app (HomeActivity)
+        else {
+            Intent i = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(i);
+            finish();
+        }
+
     }
 }
