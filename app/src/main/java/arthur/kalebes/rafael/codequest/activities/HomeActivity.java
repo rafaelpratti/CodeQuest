f73cc7a795e581166260f7432319076e26f69bba
20221TIIMI0190 <kalebe.olegario1850@gmail.com>
2024-12-19 11:28:04 -0300

    fragments mudando

--- app/src/main/java/arthur/kalebes/rafael/codequest/activities/HomeActivity.java
+++ app/src/main/java/arthur/kalebes/rafael/codequest/activities/HomeActivity.java
@@ -1,26 +1,59 @@
 package arthur.kalebes.rafael.codequest.activities;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.*;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import arthur.kalebes.rafael.codequest.R;
+import arthur.kalebes.rafael.codequest.fragments.CursosFragment;
+import arthur.kalebes.rafael.codequest.fragments.MeuProgressoFragment;
+import arthur.kalebes.rafael.codequest.fragments.MeusCursosFragment;
+import arthur.kalebes.rafael.codequest.fragments.PerfilFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Default Fragment
+        loadFragment(new CursosFragment());
+
+        // BottomNavigationView listener
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment = null;
+            if (item.getItemId() == R.id.cursos) {
+                fragment = new CursosFragment();
+            }
+
+            if (item.getItemId() == R.id.meusCursos) {
+                fragment = new MeusCursosFragment();
+            }
+
+            if (item.getItemId() == R.id.progresso) {
+                fragment = new MeuProgressoFragment();
+            }
+
+            if (item.getItemId() == R.id.perfil) {
+                fragment = new PerfilFragment();
+            }
+
+            return loadFragment(fragment);
         });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayout, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
